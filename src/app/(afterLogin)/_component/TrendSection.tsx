41b7da8b1e32adc1e4ec72a057478030270f680c
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-25 11:43:51 +0900

    feat: api전까지 완료

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,12 @@
-import style from './trendSection.module.css';
+"use client";
+
+import { usePathname } from "next/navigation";
+import style from "./trendSection.module.css";
 import Trend from "@/app/(afterLogin)/_component/Trend";
 
 export default function TrendSection() {
+  const pathname = usePathname();
+  if (pathname === `/explore`) return null;
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
@@ -18,5 +23,5 @@ export default function TrendSection() {
         <Trend />
       </div>
     </div>
-  )
+  );
 }
